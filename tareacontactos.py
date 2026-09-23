f3cb02d61053c0bc2a84409c149d6394b407796a
wnayeli055-beep <wnayeli055@gmail.com>
2026-09-23 09:25:23 -0500

    Refactor contact management program structure

--- tareacontactos.py
+++ tareacontactos.py
@@ -1,6 +1,10 @@
+#Programa: Registro de contactos
+#Estrucura: Diccionario
 
+# Crear colección de datos
 contacto= {}
 
+# Agregar datos 
 def agregar_contacto(nombre, telefono): 
     if nombre not in contacto: 
         contacto [nombre] = telefono
@@ -8,6 +12,7 @@ def agregar_contacto(nombre, telefono):
     else:
         print(f"El contacto {nombre} ya existe.")
 
+#Mostrar todos los contactos
 def mostrar_contactos():
     print("\n=== Lista de Contactos ===")
     if contacto:
@@ -16,13 +21,15 @@ def mostrar_contactos():
     else:
         print("No hay contactos registrados.")
     print("==========================\n")
-
+    
+#Operación adicional
 def buscar_contacto(nombre):
     if nombre in contacto:
         print(f"Contacto encontrado: Nombre: {nombre}, Teléfono: {contacto[nombre]}")
     else:
         print(f"El contacto {nombre} no existe.")
 
+#Ejecución del programa
 agregar_contacto("Ana", "0988876888")
 agregar_contacto("Juan", "0988844888")
 agregar_contacto("Raul", "0986544677")
